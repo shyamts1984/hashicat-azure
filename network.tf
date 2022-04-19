1e159cb8c8f6f79d5728333c3b7397b5f698b219
shyamts1984 <shyamts1984@gmail.com>
2022-04-19 14:37:29 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/shyamts1984/network/azurerm"
   version = "3.5.0"
   # insert required variables herei
-  resource_group_name = shyamts-workshop
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
